4de4568d66396439fbd3fcce790565c70a03cdf1
maldrek3103 <cpisanu96@gmail.com>
2021-10-20 18:04:30 +0200

    made seeder with faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Str;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,7 +12,17 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        for ($i = 0; $i < 50; $i++) {
+            $post = new Post();
+
+            $post->title = $faker->text(50);
+            $post->content = $faker->paragraph(2, true);
+            $post->image = $faker->imageUrl(250, 250);
+            $post->slug = Str::slug($post->title, '-');
+
+            $post->save();
+        }
     }
 }
