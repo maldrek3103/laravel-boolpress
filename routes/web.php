8c3d588e1d22e5ee67bde41dacff933a76b20d39
maldrek3103 <cpisanu96@gmail.com>
2021-10-20 17:23:47 +0200

    added vue on front-office

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
 
 Auth::routes(['register' => false]);
 
@@ -25,3 +22,8 @@ Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->
     Route::get('/', 'HomeController@index')->name('home');
     Route::resource('posts', 'PostController');
 });
+
+// Gestione rotte diverse da admin de
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*');
