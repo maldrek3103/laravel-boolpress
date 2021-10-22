75249067fccdd43ce05e4a3d8ee1c766c439bd7e
maldrek3103 <cpisanu96@gmail.com>
2021-10-22 11:39:33 +0200

    added pagination

--- resources/js/components/Posts/PostList.vue
+++ resources/js/components/Posts/PostList.vue
@@ -2,6 +2,22 @@
     <section id="post-list">
         <h4>My post list</h4>
         <PostCard v-for="post in posts" :key="post.id" :post="post"/>
+        <nav aria-label="Page navigation example">
+    <ul class="pagination">
+
+        <li class="page-item" v-if="pagination.currentPage > 1" @click="getPosts(pagination.currentPage - 1)">
+            <a class="page-link" href="#">Previous</a>
+        </li>
+
+        <li class="page-item" :class="{active: pagination.currentPage === n}" v-for="n in pagination.lastPage" :key="n" @click="getPosts(n)">
+            <a class="page-link" href="#">{{n}}</a>
+        </li>
+
+        <li class="page-item" v-if="pagination.lastPage > pagination.currentPage" @click="getPosts(pagination.currentPage + 1)">
+            <a class="page-link" href="#">Next</a>
+        </li>
+    </ul>
+</nav>
     </section>
 </template>
 
@@ -12,17 +28,22 @@ export default {
     nmae: "PostList",
     data(){
         return {
-            posts: []
+            posts: [],
+            pagination: {}
         };
     },
     components:{
         PostCard,
     },
     methods: {
-        getPosts(){
+        getPosts(page){
             axios
-            .get('http://localhost:8000/api/posts')
-            .then(res => { this.posts = res.data })
+            .get(`http://localhost:8000/api/posts?page=${page}`)
+            .then(res => { 
+                const {data, current_page, last_page} = res.data;
+                this.posts = data;
+                this.pagination = {currentPage: current_page, lastPage: last_page};
+            })
             .catch(error => {console.error(error)});
         }
     },
@@ -33,5 +54,7 @@ export default {
 </script>
 
 <style>
-
+.page-item {
+    cursor: pointer;
+}
 </style>
