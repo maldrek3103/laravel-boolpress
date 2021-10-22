361ba26b2497ec3b2b2976c9ab74f5a80452cb91
maldrek3103 <cpisanu96@gmail.com>
2021-10-22 16:09:55 +0200

    added seeder and migration foreign key for categories with dropdown

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $category_names = ['HTML', 'PHP', 'JS', 'CSS', 'VueJS', 'Laravel'];
+
+        foreach ($category_names as $name) {
+            $category = new Category();
+            $category->name = $name;
+            $category->slug = Str::slug($name, '-');
+
+            $category->save();
+        }
     }
 }
